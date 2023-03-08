8d56141c38551ccb8f20903474c81f8c3eaa0f52
EC2 Default User <ec2-user@ip-172-31-37-148.ec2.internal>
2023-03-08 20:00:23 +0000

    After completion of task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=magosla/ml_microservice
 
 # Step 2:  
 # Authenticate & tag
+docker login
 echo "Docker ID and Image: $dockerpath"
+docker --tag magosla $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
